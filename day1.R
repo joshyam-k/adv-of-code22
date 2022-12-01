464493df79a47e6763fdfd71dabfae83a0630dce
joshyam-k <yamamojo@reed.edu>
2022-11-30 21:28:47 -0800

    part 1 done

--- day1.R
+++ day1.R
@@ -3,10 +3,23 @@ library(here)
 
 input <- tibble(x = readLines(here("day1.txt")))
 
-input %>% 
+input_list <- input %>% 
   mutate(flag = ifelse(x == "", row_number(), NA)) %>% 
   fill(flag, .direction = "up") %>% 
+  filter(x != "") %>% 
+  mutate(x = as.numeric(x)) %>% 
   group_by(flag) %>% 
   group_split()
+
+select_and_add <- function(.x){
+  .x %>% 
+    select(x) %>% 
+    summarise(sum = sum(x)) %>% 
+    pull(sum)
+}
+
+input_list %>% 
+  map_dbl(.f = select_and_add) %>% 
+  max()
 
 
