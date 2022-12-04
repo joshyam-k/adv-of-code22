607dc721c95825dcd2322090d588551e4ed5ea03
joshyam-k <yamamojo@reed.edu>
2022-12-03 21:22:18 -0800

    done with day 4

--- solutions/day4.R
+++ solutions/day4.R
@@ -1,3 +1,22 @@
 library(tidyverse)
 library(here)
 
+input <- tibble(x = readLines(here("inputs", "day4.txt"))) %>% 
+  separate(x, into = c("pair1", "pair2"), sep = ",") %>% 
+  separate(pair1, into = c("pair1_1", "pair1_2"), sep = "-") %>% 
+  separate(pair2, into = c("pair2_1", "pair2_2"), sep = "-") %>% 
+  mutate_if(is.character,as.numeric) 
+  
+input %>% 
+  rowwise() %>% 
+  filter((between(pair1_1, pair2_1, pair2_2) & between(pair1_2, pair2_1, pair2_2)) |
+           (between(pair2_1, pair1_1, pair1_2) & between(pair2_2, pair1_1, pair1_2))) %>% 
+  nrow()
+
+
+input %>% 
+  rowwise() %>% 
+  filter((between(pair1_1, pair2_1, pair2_2) | between(pair1_2, pair2_1, pair2_2)) |
+           (between(pair2_1, pair1_1, pair1_2) | between(pair2_2, pair1_1, pair1_2))) %>% 
+  nrow()
+  
