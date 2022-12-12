65d1cb66632842f9f7a94e48d5dd42fb7fe0e026
joshyam-k <yamamojo@reed.edu>
2022-12-11 23:17:12 -0800

    day12

--- solutions/day12.R
+++ solutions/day12.R
@@ -1,5 +1,30 @@
 library(tidyverse)
 library(here)
+library(adventdrob)
+library(tidygraph)
 
+raw <- tibble(x = readLines(here("inputs", "day12.txt")))  %>% 
+  grid_graph(x, mutual = T, directed = T) 
 
-input <- tibble(x = readLines(here("inputs", "day12.txt"))) 
+nodes <- raw %>% 
+  as_tibble() %>% 
+  mutate(elevation = case_when(
+    value == 'S' ~ 1L,
+    value == 'E' ~ 26L,
+    T ~ match(value, letters)
+  ))
+
+input <- raw %>% 
+  activate("edges") %>% 
+  mutate(from_val = nodes$elevation[from],
+         to_val = nodes$elevation[to]) %>% 
+  filter(to_val <= from_val + 1) %>% 
+  activate("nodes") %>% 
+  mutate(distance = node_distance_to(which(nodes$value == 'E'), mode = "out"))
+
+input %>% 
+  filter(value == 'S')
+
+input %>% 
+  filter(value == 'a') %>% 
+  arrange(distance)
